7fbfedd1641b854732800aa1c8c9dae1d6b3efab
unknown <smtishkar@mail.ru>
2024-02-14 22:41:24 +0300

    добавлен регистрацию и вход (пути не настроины)

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 import myapp.views as views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('one_recepie/<int:recepie_id>',views.one_recepie, name='one_recepie')
+    path('one_recepie/<int:recepie_id>',views.one_recepie, name='one_recepie'),
+    path('signup',views.registration, name='registration'),
+    path('login/', views.sign_in, name='login')
 
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Recipe
 from random import choice
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from .forms import LoginForm
+from django.contrib import messages
 # Create your views here.
 
 
@@ -25,3 +29,41 @@ def one_recepie(request, recepie_id: int):
         'recepies': recepies
     }
     return render(request, 'myapp/one_recepie.html', context=context)
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'myapp/signup.html', {'form': form})
+
+
+def sign_in(request):
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request,'myapp/login.html', {'form': form})
+    
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request,username=username,password=password)
+            if user:
+                login(request, user)
+                # messages.success(request,f'Hi, welcome back!')
+                messages.success(request,f'Hi {username.title()}, welcome back!')
+                return redirect('/')
+        
+        # form is not valid or user is not authenticated
+        messages.error(request,f'Invalid username or password')
+        return render(request,'myapp/login.html',{'form': form})
